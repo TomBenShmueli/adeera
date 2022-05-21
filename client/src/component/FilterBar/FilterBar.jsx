ea9f72f2bfa6c65e6f4898e1bac0f1bb9622736c
גבריאל ז׳ורנו <riel@h-MacBook-Pro-sl-gbryl.local>
2022-05-21 11:37:33 +0300

    fixing the UI - work with dummy Data

--- client/src/component/FilterBar/FilterBar.jsx
+++ client/src/component/FilterBar/FilterBar.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
-import Button from "../../ui/Button/Button";
 import Filter from '../../ui/Filter/Filter';
 import './FilterBar.css';
+import Stack from '@mui/material/Stack';
 
 const FilterBar = () => {
-    const initRoomValues = [3,5]
-    const minRooms=1
-    const maxRooms=10
-    const roomSteps=1
+    const initRoomValues = [3, 8];
+    const minRooms = 1;
+    const maxRooms = 10;
+    const roomSteps = 1;
     return (
         <div className='container'>
             <nav className='side-bar'>
                 <h1>Filters</h1>
-                <div className="center">
+
+                <div className="filter">
+                    <h4 dir='rtl'>מספר חדרים</h4>
                     <Filter
-                    className='filter-rooms'
-                    initValues={initRoomValues}
-                    minValue = {minRooms}
-                    maxValue={maxRooms}
-                    steps={roomSteps}
+                        initValues={initRoomValues}
+                        minValue={minRooms}
+                        maxValue={maxRooms}
+                        steps={roomSteps}
                     />
-                    <Button className='btn-google' display='Google' />
-                    <Button className='btn-facebook' display='Facebook' />
+                </div>
+
+                <div className="filter">
+                    <h4 dir='rtl'>מחיר</h4>
+                    <Filter
+                        initValues={initRoomValues}
+                        minValue={minRooms}
+                        maxValue={maxRooms}
+                        steps={roomSteps}
+                    />
                 </div>
             </nav>
         </div>
--- client/src/component/Stage/Stage.jsx
+++ client/src/component/Stage/Stage.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import './Stage.css';
 import Apartment from "../Apartment/Apartment";
+import data from "../../data"
+
+const dummyData = data;
 
 const Stage = () => {
     return (
         <div className='stage'>
-            <Apartment />
-            <Apartment />
-            <Apartment />
-            <Apartment />
-            <Apartment />
-            <Apartment />
-            <Apartment />
-            <Apartment />
-            <Apartment />
-            <Apartment />
+            <div className="cards">
+                {dummyData.map((apt, index) => {
+                    return (
+                        <Apartment key={index} image={apt.images[1]} username={apt.username} text={apt.post_text} />
+                    )
+                })}
+            </div>
         </div>
     );
 }
