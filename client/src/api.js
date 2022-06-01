c0561f6a8aed527ace542de764e8c919f2bb2d7e
TomBenShmueli <tombenshmueli@gmail.com>
2022-06-01 15:51:01 +0300

    commented out api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,26 +1,25 @@
-import Axios from 'axios';
+// import Axios from 'axios';
 
-const serverAPIPort = 8082;
-const host = 'http://localhost'
-const APIDomain = 'apartments';
-const APIPath = `/api/${APIDomain}`;
-const APIRootPath = `${host}:${serverAPIPort}${APIPath}`;
-const staticsPort = 3000;
-const staticsUrl = `${host}:${staticsPort}/`;
+// const serverAPIPort = 8082;
+// const host = 'http://localhost'
+// const APIDomain = 'apartments';
+// const APIPath = `/api/${APIDomain}`;
+// const APIRootPath = `${host}:${serverAPIPort}${APIPath}`;
+// const staticsPort = 3000;
+// const staticsUrl = `${host}:${staticsPort}/`;
 
-// var dummyData = [];
+// // var dummyData = [];
 
-// Axios.get(APIRootPath)
-//     .then((res) => {
-//         console.log(res)
-//         dummyData = res.data
-//     });
+// // Axios.get(APIRootPath)
+// //     .then((res) => {
+// //         console.log(res)
+// //         dummyData = res.data
+// //     });
 
-export const creatApartments = () => {
-    return {
-        getApartment: () => {
-            Axios.get(APIRootPath).then((res) => res.data);
-        }
-    }
-}
-
+// export const creatApartments = () => {
+//     return {
+//         getApartment: () => {
+//             Axios.get(APIRootPath).then((res) => res.data);
+//         }
+//     }
+// }
